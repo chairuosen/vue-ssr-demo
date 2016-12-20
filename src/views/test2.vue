e26af183b4c5bea7d5bdb6cae1daee62e2148b39
ruosen.chai <chairuosen@gmail.com>
2016-12-20 15:53:14 +0800

    add store.count

--- src/views/test2.vue
+++ src/views/test2.vue
@@ -4,12 +4,19 @@
 <template>
     <div>
         This is view 2.
+        <br>
+        store.count = {{count1}}
     </div>
 </template>
 <script>
     module.exports = {
         data: function () {
             return {}
-        }
+        },
+        computed: {
+            count1(){
+                return this.$store.state.count
+            }
+        },
     }
 </script>
